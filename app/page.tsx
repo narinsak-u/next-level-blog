051133260ae051787ab69658825751f9ca465b1a
narinsak.u <nawongsak@gmail.com>
2026-04-13 12:54:22 +0700

    feat: add mode selection toggle for Focused/Jianghu home page views

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-// import HomePage from "@/components/home/HomePage";
+"use client";
+
+import HomePage from "@/components/home/HomePage";
 import NewProfile from "@/components/home/NewProfile";
+import ModeSelector from "@/components/home/ModeSelector";
+import { useModeStore } from "@/hooks/use-mode-store";
 
-const Home = async () => {
-  // return <HomePage />;
+const Home = () => {
+  const mode = useModeStore((state) => state.mode);
 
-  return <NewProfile />;
+  return (
+    <>
+      <ModeSelector />
+      {mode === "focused" ? <HomePage /> : <NewProfile />}
+    </>
+  );
 };
 
 export default Home;
