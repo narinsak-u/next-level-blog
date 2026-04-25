2df836e3b2b183e3bed4da7a8d4bbdd8a042ffea
narinsak.u <nawongsak@gmail.com>
2026-04-26 00:25:34 +0700

    feat: add PostHeatmap component with Cal-Heatmap

---
+++ types/cal-heatmap.d.ts
@@ -0,0 +1,15 @@
+declare module "cal-heatmap" {
+  export default class CalHeatmap {
+    paint(options: any, plugins?: any[]): void;
+    destroy(): void;
+  }
+}
+
+declare module "cal-heatmap/plugins/Tooltip" {
+  export default class Tooltip {}
+}
+
+declare module "cal-heatmap/cal-heatmap.css" {
+  const css: string;
+  export default css;
+}
